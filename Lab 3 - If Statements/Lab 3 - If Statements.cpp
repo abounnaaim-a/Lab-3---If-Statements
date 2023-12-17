713a17376585edee0855fd392fe67647f739ebff
abounnaaim-a <aabounna@purdue.edu>
2023-12-16 19:10:19 -0500

    Chicken nugget deluxe

--- Lab 3 - If Statements/Lab 3 - If Statements.cpp
+++ Lab 3 - If Statements/Lab 3 - If Statements.cpp
@@ -2,19 +2,59 @@
 //
 
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Divison" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value: ";
+	cin >> A;
+	cout << "Please enter the second value: ";
+	cin >> B;
+	cout << " " << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void addition(float A, float B) {
+	float result = A + B;
+	cout << A << " + " << B << " = " << result << endl;
+}
+void subtraction(float A, float B) {
+	float result = A - B;
+	cout << A << " - " << B << " = " << result << endl;
+}
+void multiplication(float A, float B) {
+	float result = A * B;
+	cout << A << " * " << B << " = " << result << endl;
+}
+void division(float A, float B) {
+	float result = A / B;
+	cout << A << " / " << B << " = " << result << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		addition(A, B);
+	}
+	if (choice == 2) {
+		subtraction(A, B);
+	}
+	if (choice == 3) {
+		multiplication(A, B);
+	}
+	if (choice == 4) {
+		division(A, B);
+	}
+	return 0;
+}
